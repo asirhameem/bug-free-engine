eaddfa14626bd4805ff431f193e934f6ce3f9976
Asir Hameem <asirhameem@gmail.com>
2023-11-18 02:16:57 +0600

    #chore: added dotenv and configs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express, {Request, Response} from "express";
-
+import configs from "./configs/index";
 const app =express();
 
 app.get('/', (req: Request, res: Response) => {
@@ -7,5 +7,6 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.listen(3000, () => {
+    console.log(configs);
     console.log("application listening on port 3000");
 })
