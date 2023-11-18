6a9991c274c46e08fc748356d80029d50debd4c5
Asir Hameem <asirhameem@gmail.com>
2023-11-18 23:08:20 +0600

    #chore: added uuid

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,13 @@
 import AppDataSource from "../configs/orm.config";
 import UserEntity from "../entities/user.entity";
 import {User} from "../interfaces/user.interface";
+import {v4 as uuid} from "uuid";
+
 
 const userRepo = AppDataSource.getRepository(UserEntity);
 export const createUser = async (user: User) => {
   let entity : UserEntity = new UserEntity();
+  entity.user_id = uuid();
   entity.name = user.name;
   entity.email = user.email;
   entity.password = user.password;
